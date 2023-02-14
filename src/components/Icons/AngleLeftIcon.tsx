8c2a308f1b7a6fafaf04ac994ed8b8203aee3676
zhengji.su <367265893@qq.com>
2023-02-14 18:54:54 +0800

    feat: 1. 新增AngleLeftIcon、AngleRightIcon、TickIcon图标 2. 新增CategoryList组件 3. 完成开发Category组件

---
+++ src/components/Icons/AngleLeftIcon.tsx
@@ -0,0 +1,11 @@
+function AngleLeftIcon() {
+
+  return (
+    <svg className="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium MuiPaginationItem-icon css-k2nkbn" focusable="false"
+         aria-hidden="true" viewBox="0 0 24 24" data-testid="NavigateBeforeIcon">
+      <path d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z"></path>
+    </svg>
+  )
+}
+
+export default AngleLeftIcon
---
+++ src/components/Icons/TickIcon.tsx
@@ -0,0 +1,16 @@
+function TickIcon() {
+
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img"
+         className="MuiBox-root css-3o0h5k iconify iconify--eva" width="1em" height="1em" viewBox="0 0 24 24">
+      <g id="iconifyReact2301">
+        <g id="iconifyReact2302">
+          <path id="iconifyReact2303" fill="currentColor"
+                d="M9.86 18a1 1 0 0 1-.73-.32l-4.86-5.17a1 1 0 1 1 1.46-1.37l4.12 4.39l8.41-9.2a1 1 0 1 1 1.48 1.34l-9.14 10a1 1 0 0 1-.73.33Z"></path>
+        </g>
+      </g>
+    </svg>
+  )
+}
+
+export default TickIcon
